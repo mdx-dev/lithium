81d8f4c7d3b14c432eefb514b15278f6fe732e6f
Brandon Westcott <brandon.westcott@vitals.com>
2014-07-08 10:21:32 -0400

    Update for mongo driver 1.5.4

--- tests/mocks/data/source/MockMongoConnection.php
+++ tests/mocks/data/source/MockMongoConnection.php
@@ -2,16 +2,80 @@
 /**
  * Lithium: the most rad php framework
  *
- * @copyright     Copyright 2012, Union of RAD (http://union-of-rad.org)
+ * @copyright     Copyright 2014, Union of RAD (http://union-of-rad.org)
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
 namespace lithium\tests\mocks\data\source;
 
-class MockMongoConnection extends \lithium\data\source\MongoDb {
+use MongoId;
+
+class MockMongoConnection {
+
+	public $queries = array();
+
+	public $results = array();
+
+	protected $_collection = null;
+
+	public $gridFsPrefix = null;
 
 	public function connect() {
 		return false;
+	}
+
+	public function &__get($property) {
+		$this->_collection = $property;
+		return $this;
+	}
+
+	public function listDBs() {
+		return array();
+	}
+
+	public function getConnections() {
+		return array(array(
+			'hash' => 'localhost:27017;-;X;56052',
+			'server' => array(),
+			'connection' => array()
+		));
+	}
+
+	public function insert(array &$data, array $options = array()) {
+		$data['_id'] = new MongoId();
+		return $this->_record(__FUNCTION__, compact('data', 'options'));
+	}
+
+	protected function _record($type, array $data = array()) {
+		$collection = $this->_collection;
+		$this->queries[] = compact('type', 'collection') + $data;
+		$result = array_pop($this->results);
+		return $result === null ? false : $result;
+	}
+
+	public function update($conditions, $update, $options) {
+		return $this->_record(__FUNCTION__, compact('conditions', 'update', 'options'));
+	}
+
+	public function remove($conditions, $options) {
+		return $this->_record(__FUNCTION__, compact('conditions', 'options'));
+	}
+
+	public function find($conditions, $fields) {
+		return $this->_record(__FUNCTION__, compact('conditions', 'fields'));
+	}
+
+	public function listCollections() {
+		return $this->_record(__FUNCTION__);
+	}
+
+	public function getGridFS($prefix = "fs") {
+		$this->gridFsPrefix = $prefix;
+		return $this;
+	}
+
+	public function storeBytes($bytes = null, array $extra = array(), array $options = array()) {
+		return;
 	}
 }
 
